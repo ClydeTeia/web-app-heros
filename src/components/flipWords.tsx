3a99be29c24a69911424094cb18cde69829adb6d
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2024-06-24 15:21:47 +0800

    added jose rizal visit path

--- src/components/flipWords.tsx
+++ src/components/flipWords.tsx
@@ -7,16 +7,9 @@ export default function FlippingWords() {
   const words = ["Teddie", "Manaf", "Ian"];
 
   return (
-    <div className="h-full relative w-full overflow-hidden flex flex-col items-center justify-center rounded-lg">
-      <div className="absolute inset-0 w-full h-full bg-slate-900 z-20  pointer-events-none" />
-      <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>
-        <div className="h-[25rem] flex justify-center items-center px-4">
-          <div className="text-4xl mx-auto font-normal text-white dark:text-neutral-400">
-            <FlipWords words={words} /> <br />
-            are modern heroes
-          </div>
-        </div>
-      </h1>
+    <div className="text-4xl mx-auto font-normal text-white dark:text-neutral-400">
+      <FlipWords words={words} /> <br />
+      are modern heroes
     </div>
   );
 }
