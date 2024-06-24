9d9c0add7b698a6888c92838fb1ad6e412b2854f
Nobodyknowsss <124364728+Nobodyknowsss@users.noreply.github.com>
2024-06-24 14:24:37 +0800

    [Manaf][moving cards to put our answer]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import FlippingWords from "@/components/flipWords";
 import { GlobeHeroesLocations } from "@/components/globe";
 import Image from "next/image";
-
+import Section3 from "@/components/section3";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <FlippingWords />
       <GlobeHeroesLocations />
+      <Section3 />
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,12 @@
-import type { Config } from "tailwindcss";
+const { Config } = require("tailwindcss");
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
-const config: Config = {
+/** @type {Config} */
+module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -17,18 +20,28 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      animation: {
+        scroll:
+          "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
+      },
+      keyframes: {
+        scroll: {
+          to: {
+            transform: "translate(calc(-50% - 0.5rem))",
+          },
+        },
+      },
     },
   },
   plugins: [addVariablesForColors],
 };
-export default config;
 
-function addVariablesForColors({ addBase, theme }: any) {
+function addVariablesForColors({ addBase, theme }) {
   let allColors = flattenColorPalette(theme("colors"));
   let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
   );
- 
+
   addBase({
     ":root": newVars,
   });
