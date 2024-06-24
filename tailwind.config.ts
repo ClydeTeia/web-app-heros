0dcd83c9c62c52d5c1fa110b428fa0f767a86f83
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2024-06-24 14:48:27 +0800

    fixed missing export

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,24 +1,18 @@
-
-const { Config } = require("tailwindcss");
+import type { Config } from "tailwindcss";
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 
-import type { Config } from "tailwindcss";
-
 const svgToDataUri = require("mini-svg-data-uri");
-
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
-/** @type {Config} */
-module.exports = {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-
   plugins: [
     addVariablesForColors,
     function ({ matchUtilities, theme }: any) {
@@ -32,12 +26,12 @@ module.exports = {
         },
         { values: flattenColorPalette(theme("backgroundColor")), type: "color" }
       );
-
     },
   ],
 };
+export default config;
 
-function addVariablesForColors({ addBase, theme }) {
+function addVariablesForColors({ addBase, theme }: any) {
   let allColors = flattenColorPalette(theme("colors"));
   let newVars = Object.fromEntries(
     Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
